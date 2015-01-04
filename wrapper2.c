8a899a8b00a64994eecf46c68121773de10090bc
Arun <arun_rm@rediffmail.com>
2015-01-04 19:56:05 +0530

    modified wrapper2.c to fix the build error and improve the logicwq

--- wrapper2.c
+++ wrapper2.c
@@ -1,9 +1,10 @@
 #include<stdio.h> 
 void main()
 {
-	printf("Enter value of a");
-	scanf(%d,a);
-	printf("Enter value of b");
-	scanf(%d,b);
+	int a,b;
+	printf("Enter value of a ");
+	scanf("%d\n",&a);
+	printf("Enter value of b ");
+	scanf("%d\n",&b);
 	printf("Sum of a and b is %d", a+b);
 }
